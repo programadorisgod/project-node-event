10e5375c6e1f2ac627f92ab6fffd9e61917ce9b5
programadorisgod <catapiascastro@gmail.com>
2024-07-03 17:32:13 -0500

    1.1.2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const ticketManager = new TicketManager(40)
 const emailService = new EmailService
 
 const testEmail = [
-    { email: "catapiascastro@gmail.com", price: 9.99 },
-    { email: "kadire30@outlook.com", price: 14.50 },
-    { email: "jesmanuelber1@gmail.com", price: 5.75 },
+    { email: "testeventemmiter1@gmail.com", price: 9.99 },
+    { email: "testeventemmiter20@outlook.com", price: 14.50 },
+    { email: "testeventemmiter2@gmail.com", price: 5.75 },
 ]
 
 ticketManager.on("buy", async (...props) => {
